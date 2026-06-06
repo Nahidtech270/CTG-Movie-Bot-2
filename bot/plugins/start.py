e532d7a13b8094ac9e8fdf0ff403699e3494a332
Nadiya2121 <misshadiya21@gmail.com>
2026-06-06 20:53:18 +0600

    Delete bot/plugins directory

--- bot/plugins/start.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-@Client.on_message(filters.command("start") & filters.private)
-async def start_handler(client: Client, message: Message):
-    await message.reply_text(
-        f"হ্যালো {message.from_user.mention},\n"
-        "আমি মুভি সার্চ বট। আপনার কাঙ্ক্ষিত মুভিটির নাম লিখে সার্চ করুন।"
-    )
