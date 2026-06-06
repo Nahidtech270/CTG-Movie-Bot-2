7e4d81c5be3f2c99ebe6ac0f79fcc8ab33726150
Nadiya2121 <misshadiya21@gmail.com>
2026-06-06 22:28:53 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # config.py
 
-API_ID = 1234567          # আপনার Telegram API ID (integer)
-API_HASH = "your_api_hash_here"  # আপনার Telegram API Hash (string)
-BOT_TOKEN = "your_bot_token_here"  # BotFather থেকে পাওয়া টোকেন
-ADMIN_ID = 123456789      # আপনার নিজের টেলিগ্রাম ইউজার আইডি
-MAIN_CHANNEL_ID = -1001234567890  # আপনার প্রধান চ্যানেলের আইডি
+API_ID = 29462738          # আপনার Telegram API ID (integer)
+API_HASH = "297f51aaab99720a09e80273628c3c24"  # আপনার Telegram API Hash (string)
+BOT_TOKEN = "8970571544:AAHkBPmDzF9crRDbeCq6-Z3RNqDpEe7i9QA"  # BotFather থেকে পাওয়া টোকেন
+ADMIN_ID = 8297458824      # আপনার নিজের টেলিগ্রাম ইউজার আইডি
+MAIN_CHANNEL_ID = -1003973741130  # আপনার প্রধান চ্যানেলের আইডি
 
 # --- ডাবল ডাটাবেজ কনফিগারেশন ---
-DATABASE_URI = "mongodb+srv://..."  # আপনার ১ম (Primary) MongoDB লিঙ্ক
-MULTIPLE_DB = True                 # ২য় ডাটাবেজ সচল করতে True রাখুন, না চাইলে False
+DATABASE_URI = "mongodb+srv://hahema9427:hahema9427@cluster0.3mf49.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # আপনার ১ম (Primary) MongoDB লিঙ্ক
+MULTIPLE_DB = False                 # ২য় ডাটাবেজ সচল করতে True রাখুন, না চাইলে False
 DATABASE_URI2 = "mongodb+srv://..." # আপনার ২য় (Backup) MongoDB লিঙ্ক (ঐচ্ছিক)
