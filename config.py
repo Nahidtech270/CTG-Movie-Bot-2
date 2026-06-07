cf678a4ca8c3b240eebd909f82a58d1046ca62ff
Nadiya2121 <misshadiya21@gmail.com>
2026-06-07 12:11:43 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = 29462738          # আপনার Telegram API ID (integer)
 API_HASH = "297f51aaab99720a09e80273628c3c24"  # আপনার Telegram API Hash (string)
 BOT_TOKEN = "8970571544:AAHkBPmDzF9crRDbeCq6-Z3RNqDpEe7i9QA"  # BotFather থেকে পাওয়া টোকেন
 ADMIN_ID = 8297458824      # আপনার নিজের টেলিগ্রাম ইউজার আইডি
-MAIN_CHANNEL_ID = -1003973741130  # প্রধান মুভি চ্যানেল আইডি (ফোর্স সাবস্ক্রিপশন)
+MAIN_CHANNEL_ID = -1002439983925  # প্রধান মুভি চ্যানেল আইডি (ফোর্স সাবস্ক্রিপশন)
 
 # --- ডাবল ডাটাবেজ কনফিগারেশন ---
 DATABASE_URI = "mongodb+srv://hahema9427:hahema9427@cluster0.3mf49.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # আপনার ১ম MongoDB লিঙ্ক
@@ -12,13 +12,20 @@ MULTIPLE_DB = False
 DATABASE_URI2 = "mongodb+srv://..." 
 
 # --- আর্নিং ও মিনি অ্যাপ কনফিগারেশন ---
-BOT_USERNAME = "CTGMovieBot"  # আপনার বটের ইউজারনেম ( শুরুতে @ দেবেন না)
+BOT_USERNAME = "CTGMovieBot"  # বটের ইউজারনেম ( শুরুতে @ দেবেন না)
 WEB_URL = "gorgeous-donetta-nahidcrk-7b84dba9.koyeb.app/view/CTG-Movie-Bot"  # Koyeb অ্যাপের লিংক ( শুরুতে https:// দেবেন না)
 
-# আপনার আয়ের ডিরেক্ট অ্যাড লিংক (এখানে Adsterra-র আসল লিংক বসানো হয়েছে)
+# আপনার আয়ের ডিরেক্ট অ্যাড লিংকসমূহ (Adsterra এবং OMG10 এর সকল ডিরেক্ট লিংক সফলভাবে যুক্ত করা হয়েছে)
 DIRECT_AD_LINKS = [
+    "https://www.effectivecpmnetwork.com/p4bm30ss3?key=8bb102e9258871570c79a9a90fa3cf9f",
+    "https://www.effectivecpmnetwork.com/q5cpmxwy44?key=075b9f116b4174922cadfae2d3291743",
+    "https://www.effectivecpmnetwork.com/c90zejmfrg?key=45a67d2f1523ee6b3988c4cc8f764a35",
+    "https://www.effectivecpmnetwork.com/a37vi5p8z?key=921db14d454c2b0e780ab51966b651a6",
     "https://www.effectivecpmnetwork.com/m30pd1efbm?key=e7b1df640000086bd44393a929d6ebab",
-    "https://www.effectivecpmnetwork.com/jxuaz5zk?key=6faf197d33f98bcb3bc070b020c19664"
+    "https://www.effectivecpmnetwork.com/jxuaz5zk?key=6faf197d33f98bcb3bc070b020c19664",
+    "https://omg10.com/4/10136724",
+    "https://omg10.com/4/10395944",
+    "https://omg10.com/4/11063339"
 ]
 
 # --- আপনার নিজস্ব প্রমোশনাল চ্যানেল লিংকসমূহ ---
